7bc5b16109403a5acaf0088e1581cefd58815b37
Schyler Hosler <schylerhosler@gmail.com>
2016-09-25 20:53:26 -0400

    Initial commit: React.js introduction to concept of event pooling

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const app = "I don't do much.";
+
+export default class Clicker extends React.Component {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    console.log(event.type); // prints 'click'
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>Click me!</button>
+    );
+  }
+}
